0727716963c195a26158fb1700e321e489106d8c
kangpangpang <kangpangpang@gmail.com>
2013-03-16 20:57:22 +0800

    support uniq browser provider

--- example/selenium/queenConfig.js
+++ example/selenium/queenConfig.js
@@ -1,13 +1,20 @@
 module.exports = {
-	script: require('../chance.js'),
-	populator: {
-		type: "selenium",
-		config: {
-			host: 'localhost', 
-			port: 4444
-		},
-		clients: [
-			{ browserName: "firefox" }
-		]
-	}
+    capture:"10.15.52.87:9000",
+    populator: {
+        type: "selenium",
+        config: {
+            host: '10.15.52.87', 
+            port: 4444
+        },
+        clients: [
+            { browserName: "iexplorer", version: "6" },
+            { browserName: "iexplorer", version: "7" },
+            { browserName: "iexplorer", version: "8" },
+            { browserName: "iexplorer", version: "9" },
+            { browserName: "chrome"},
+            { browserName: "firefox"},
+            { browserName: "opera"},
+            { browserName: "safari"}
+        ]
+    }
 };
