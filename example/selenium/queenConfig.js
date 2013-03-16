72b07029118b3613eab3343922ae1c7b66330730
kangpangpang <kangpangpang@gmail.com>
2013-03-16 21:25:54 +0800

    update queen config

--- example/selenium/queenConfig.js
+++ example/selenium/queenConfig.js
@@ -1,20 +1,13 @@
 module.exports = {
-    capture:"10.15.52.87:9000",
+    capture:"localhost:9000",
     populator: {
         type: "selenium",
         config: {
-            host: '10.15.52.87', 
+            host: 'localhost', 
             port: 4444
         },
         clients: [
-            { browserName: "iexplorer", version: "6" },
-            { browserName: "iexplorer", version: "7" },
-            { browserName: "iexplorer", version: "8" },
-            { browserName: "iexplorer", version: "9" },
-            { browserName: "chrome"},
-            { browserName: "firefox"},
-            { browserName: "opera"},
-            { browserName: "safari"}
+            { browserName: "firefox"}
         ]
     }
 };
